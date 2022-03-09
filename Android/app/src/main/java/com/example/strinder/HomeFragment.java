0306b6632efc86a10bc3890a94a749c9aaa47671
Liam Andersson <33359876+Dubstepzedd@users.noreply.github.com>
2022-03-09 10:09:00 +0100

    Began working on a navbar

---
+++ Android/app/src/main/java/com/example/strinder/HomeFragment.java
@@ -0,0 +1,49 @@
+package com.example.strinder;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link HomeFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class HomeFragment extends Fragment {
+
+    //TODO https://www.youtube.com/watch?v=fODp1hZxfng&ab_channel=CodePalace
+    /* We have to solve issue when it comes to not showing the menu when logging in.*/
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     * @return A new instance of fragment HomeFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static HomeFragment newInstance() {
+        return new HomeFragment();
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        return view;
+    }
+}

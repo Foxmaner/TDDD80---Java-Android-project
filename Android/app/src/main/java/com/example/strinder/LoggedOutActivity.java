8edf9b600c2e8ed3be10df17b108a1eb2cc04db5
Liam Andersson <33359876+Dubstepzedd@users.noreply.github.com>
2022-04-05 18:13:55 +0200

    Changes ActionBar design, and created seperate intents.

--- Android/app/src/main/java/com/example/strinder/LoggedOutActivity.java
+++ Android/app/src/main/java/com/example/strinder/LoggedOutActivity.java
@@ -2,21 +2,26 @@ package com.example.strinder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-import com.example.strinder.logged_in.LoggedInFragment;
-import com.example.strinder.logged_out.LoggedOutFragment;
+import com.example.strinder.logged_out.LoginFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class LoggedOutActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_logged_out);
 
-        LoggedOutFragment fragment = LoggedOutFragment.newInstance();
+        LoginFragment fragment = LoginFragment.newInstance();
 
         getSupportFragmentManager().beginTransaction().add(R.id.mainView,fragment).commit();
 
+
+
     }
+
 }

494ad08ed30a20954953deb74964fd891610c72d
Liam Andersson <liaan514@student.liu.se>
2022-04-25 15:54:06 +0200

    Added title for fragments and some design on profile

--- Android/app/src/main/java/com/example/strinder/LoggedInActivity.java
+++ Android/app/src/main/java/com/example/strinder/LoggedInActivity.java
@@ -3,17 +3,13 @@ package com.example.strinder;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
-import android.view.MenuItem;
+import android.widget.TextView;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 import com.example.strinder.logged_in.AddActivityFragment;
 import com.example.strinder.logged_in.FriendsFragment;
 import com.example.strinder.logged_in.HomeFragment;
@@ -21,45 +17,46 @@ import com.example.strinder.logged_in.MessagesFragment;
 import com.example.strinder.logged_in.ProfileFragment;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.navigation.NavigationBarView;
 
-import org.json.JSONException;
-import org.json.JSONObject;
-import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.Map;
 
 /** This Activity class handles all the fragments and backend code that is used when a user
  * is logged in to the server.
  */
 public class LoggedInActivity extends AppCompatActivity {
 
-    private GoogleSignInAccount account;
-    private String token;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_logged_in);
         //Get user information and JWT Token.
-        account = getIntent().getParcelableExtra("account");
-        token = getIntent().getExtras().getString("token");
+        GoogleSignInAccount account = getIntent().getParcelableExtra("account");
+        String token = getIntent().getExtras().getString("token");
 
         //Top Nav
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        ActionBar bar =  getSupportActionBar();
+
+        //Remove app name from top nav.
+        if(bar != null) {
+            getSupportActionBar().setDisplayShowTitleEnabled(false);
+        }
+
+        TextView view = toolbar.findViewById(R.id.fragmentName);
+        view.setText(getString(R.string.navbar_home));
 
         BottomNavigationView menuBar = findViewById(R.id.navBar);
         menuBar.setSelectedItemId(R.id.home);
 
-        setBottomNavListener(menuBar);
+        setBottomNavListener(toolbar,menuBar);
 
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public boolean onCreateOptionsMenu(final Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.nav_top,menu);
+
         return true;
     }
 
@@ -67,7 +64,7 @@ public class LoggedInActivity extends AppCompatActivity {
      * the user presses.
      * @param menuBar -  the BottomNavigationView object that the listener will be added to.
      */
-    private void setBottomNavListener(final BottomNavigationView menuBar) {
+    private void setBottomNavListener(final Toolbar toolbar,final BottomNavigationView menuBar) {
         menuBar.setOnItemSelectedListener(item -> {
 
             Fragment fragment = null;
@@ -75,19 +72,29 @@ public class LoggedInActivity extends AppCompatActivity {
 
             //We can't convert this to a switch case due to the ids not being final.
             if (id == R.id.home) {
+                TextView view = toolbar.findViewById(R.id.fragmentName);
+                view.setText(getString(R.string.navbar_home));
                 fragment = new HomeFragment();
             }
             else if (id == R.id.friends) {
                 fragment = new FriendsFragment();
+                TextView view = toolbar.findViewById(R.id.fragmentName);
+                view.setText(getString(R.string.navbar_friends));
             }
             else if (id == R.id.plus) {
                 fragment = new AddActivityFragment();
+                TextView view = toolbar.findViewById(R.id.fragmentName);
+                view.setText(getString(R.string.navbar_plus));
             }
             else if (id == R.id.messages) {
                 fragment = new MessagesFragment();
+                TextView view = toolbar.findViewById(R.id.fragmentName);
+                view.setText(getString(R.string.navbar_messages));
             }
             else if (id == R.id.profile) {
                 fragment = new ProfileFragment();
+                TextView view = toolbar.findViewById(R.id.fragmentName);
+                view.setText(getString(R.string.navbar_profile));
             }
 
             if(fragment != null) {

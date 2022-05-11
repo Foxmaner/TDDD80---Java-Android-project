a1dd6e2f56c933309deb41bd13e23ad51a7419e8
Eskil Brännerud <eskbr129@student.liu.se>
2022-05-11 09:30:48 +0200

    now closes keyboard on enter button

--- Android/app/src/main/java/com/example/strinder/logged_in/FriendsFragment.java
+++ Android/app/src/main/java/com/example/strinder/logged_in/FriendsFragment.java
@@ -1,9 +1,14 @@
 package com.example.strinder.logged_in;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.fragment.app.Fragment;
 
@@ -24,6 +29,8 @@ public class FriendsFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private EditText searchFriendText;
 
     public FriendsFragment() {
         // Required empty public constructor
@@ -60,6 +67,23 @@ public class FriendsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_friends, container, false);
+        View v  = inflater.inflate(R.layout.fragment_friends, container, false);
+        EditText searchFriendText = (EditText)v.findViewById(R.id.searchFriendText);
+        searchFriendText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    // When you have pressed "done/enter" on keyboard"
+                    //Closes keyboard
+                    InputMethodManager imm = (InputMethodManager) v.getContext().getSystemService(getActivity().INPUT_METHOD_SERVICE);
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+                    handled = true;
+                }
+                return handled;
+            }
+        });
+
+        return v;
     }
 }
